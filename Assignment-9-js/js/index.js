1939a123bc05ce31684de1ed194ac07767853cb8
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-11-03 16:03:33 +0200

    first four questions

--- Assignment-9-js/js/index.js
+++ Assignment-9-js/js/index.js
@@ -1,1 +1,67 @@
 // NOTE:- Q1
+
+/* var num = prompt('Please inter a number');
+if (!num || num === '' || num === ' ' || isNaN(+num))
+  console.log('enter a valid number');
+else {
+  num = +num;
+  console.log(num);
+} */
+
+// NOTE:- Q2
+
+/* var num = prompt('Please inter a number');
+if (!num || num === '' || num === ' ' || isNaN(+num))
+  console.log('enter a valid number');
+else {
+  num = +num;
+  if (num % 3 === 0 && num % 4 === 0) {
+    console.log('yes');
+  } else {
+    console.log('no');
+  }
+} */
+
+// NOTE:- Q3
+
+/* var num1 = prompt('Please inter a number 1: ');
+var num2 = prompt('Please inter a number 2: ');
+if (
+  !num1 ||
+  num1 === '' ||
+  num1 === ' ' ||
+  isNaN(+num1) ||
+  !num2 ||
+  num2 === '' ||
+  num2 === ' ' ||
+  isNaN(+num2)
+)
+  console.log('enter a valid num1ber');
+else {
+  num1 = +num1;
+  num2 = +num2;
+  var max = num1;
+
+  if (num2 === max) {
+    console.log('enter non equal numbers');
+  } else if (num2 > max) {
+    console.log(num2);
+  } else console.log(max);
+}
+ */
+
+// NOTE:- Q4
+
+/* var num = prompt('Please inter a number');
+if (!num || num === '' || num === ' ' || isNaN(+num))
+  console.log('enter a valid number');
+else {
+  num = +num;
+  if (num > 0) {
+    console.log('Positive');
+  } else if (num < 0) {
+    console.log('Negative');
+  } else {
+    console.log('Zero is not Positive nor Negative');
+  }
+} */
