a94bb8bda8eefc1896983640d7826550462c5474
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-11-17 21:04:50 +0200

    design the app

---
+++ CRUD-system/js/index.js
@@ -0,0 +1,20 @@
+const productName = document.querySelector('#product-name');
+const productPrice = document.querySelector('#product-price');
+const productCategory = document.querySelector('#product-category');
+const productImg = document.querySelector('#product-image');
+const productDescription = document.querySelector('#product-description');
+
+const addProductBtn = document.querySelector('#add-product');
+const updateFormBtn = document.querySelector('#update-product-form');
+
+const updateCardBtn = document.querySelector('#update-card');
+const deleteProductBtn = document.querySelector('#delete-product');
+
+/* 
+const myFile = new File([''], 'lab-1.jpg', {
+    type: 'image/*',
+  });
+  const dataTransfer = new DataTransfer();
+  dataTransfer.items.add(myFile);
+  productImg.files = dataTransfer.files;
+*/
