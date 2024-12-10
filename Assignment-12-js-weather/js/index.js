5eb8e4867eb728db92eaa19d9caa76ecbe170523
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-12-10 22:02:58 +0200

    finish the project

--- Assignment-12-js-weather/js/index.js
+++ Assignment-12-js-weather/js/index.js
@@ -1,11 +1,72 @@
 import { formateDate } from './utils.js';
-/* 
-API Key: 6d8fd84e6bae4d9087b102341241012
-Base URL: http://api.weatherapi.com/v1
-*/
-const API_KEY = '6d8fd84e6bae4d9087b102341241012';
-const BASE_URL = 'http://api.weatherapi.com/v1';
-const city = 'Cairo';
+import { getForecast } from './utils.js';
 
-const SEARCH_URL = `${BASE_URL}/search.json?key=${API_KEY}&q=${city}`;
-const FORCAST_URL = `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=3&hour=0`;
+const searchIput = document.querySelector('#searchIput');
+const weekDayEl = document.querySelector('#weekDay');
+const dayAndMonth = document.querySelector('#dayAndMonth');
+const locationName = document.querySelector('#locationName');
+const degree = document.querySelector('#degree');
+const iconImg = document.querySelector('#iconImg');
+const dayStatus = document.querySelector('#dayStatus');
+
+const rain = document.querySelector('#rain');
+const wind = document.querySelector('#wind');
+const windDir = document.querySelector('#windDir');
+
+// TODO: get Current forecast
+function currentForecast(data) {
+  const { location, current } = data;
+  const { name } = location;
+  const { temp_c, last_updated, wind_dir, wind_kph, precip_mm } = current;
+  const { text, icon } = current.condition;
+  const [weekDay, day, month] = formateDate(last_updated);
+
+  weekDayEl.innerHTML = weekDay;
+  dayAndMonth.innerHTML = day + month;
+  locationName.innerHTML = name;
+  degree.innerHTML = `${temp_c}&deg;C`;
+  iconImg.src = icon;
+  dayStatus.innerHTML = text;
+  rain.innerHTML = precip_mm + ' mm';
+  wind.innerHTML = wind_kph + 'Km/h';
+  windDir.innerHTML = wind_dir;
+}
+// TODO: get next two days forecast
+function nextTwoDaysForecaset(data) {
+  const { forecastday } = data.forecast;
+
+  for (let i = 1; i < forecastday.length; i++) {
+    const { date, day } = forecastday[i];
+    const { maxtemp_c, mintemp_c, condition } = day;
+    const { text, icon } = condition;
+    const [weekDay] = formateDate(date);
+
+    document.querySelector(`#weekDay-${i}`).innerHTML = weekDay;
+    document.querySelector(`#iconImg-${i}`).src = icon;
+
+    document.querySelector(`#maxDegree-${i}`).innerHTML = `${maxtemp_c}&deg;C`;
+
+    document.querySelector(`#minDegree-${i}`).innerHTML = `${mintemp_c}&deg;C`;
+
+    document.querySelector(`#weekDay-${i}`).innerHTML = weekDay;
+
+    document.querySelector(`#dayStatus-${i}`).innerHTML = text;
+  }
+}
+// TODO: display full Wather
+async function displayWeather(value) {
+  const forecastData = await getForecast(value);
+  currentForecast(forecastData);
+  nextTwoDaysForecaset(forecastData);
+}
+
+// TODO: search cities
+
+searchIput.addEventListener('keyup', async () => {
+  const value = searchIput.value;
+
+  if (value.length < 3) return;
+  displayWeather(value);
+});
+
+displayWeather('Cairo');
--- Assignment-12-js-weather/js/utils.js
+++ Assignment-12-js-weather/js/utils.js
@@ -1,9 +1,30 @@
-export function formateDate() {
+const API_KEY = '6d8fd84e6bae4d9087b102341241012';
+const BASE_URL = 'http://api.weatherapi.com/v1';
+
+export function formateDate(value) {
   const date = new Intl.DateTimeFormat('en-GB', {
     dateStyle: 'full',
   })
-    .format(new Date('2024-12-10'))
+    .format(new Date(value))
     .split(' ');
 
   return date;
+}
+
+export async function getCity(city) {
+  const res = await fetch(`${BASE_URL}/search.json?key=${API_KEY}&q=${city}`);
+  const data = await res.json();
+
+  return data[0]?.name;
+}
+
+export async function getForecast(value) {
+  const city = await getCity(value);
+
+  const res = await fetch(
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=3&hour=0`
+  );
+  const data = await res.json();
+
+  return data;
 }
