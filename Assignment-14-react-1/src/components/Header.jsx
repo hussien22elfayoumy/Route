eb4e06e25e697027ad273fff76683fea896af184
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-01-06 21:04:26 +0200

    feat: implemnet contact  page

--- Assignment-14-react-1/src/components/Header.jsx
+++ Assignment-14-react-1/src/components/Header.jsx
@@ -4,9 +4,9 @@ export default function Header({ title, color, bgColor }) {
   return (
     <header className={`flex flex-col items-center gap-5 text-${color}`}>
       <h1 className="text-4xl font-bold uppercase">{title}</h1>
-      <div className={`relative h-1 w-[60%] bg-${color}`}>
+      <div className={`relative h-1 w-[200px] bg-${color}`}>
         <FaStar
-          className={`bg-${bgColor} absolute left-[50%] top-[50%] size-8 translate-x-[-50%] translate-y-[-50%] rounded-full p-1`}
+          className={`bg-${bgColor} absolute left-[50%] top-[50%] size-10 translate-x-[-50%] translate-y-[-50%] rounded-full p-2`}
         />
       </div>
     </header>
--- Assignment-14-react-1/src/pages/Contact.jsx
+++ Assignment-14-react-1/src/pages/Contact.jsx
@@ -1,3 +1,40 @@
+import Header from "../components/Header";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="bg-zinc-50">
+      <div className="container mx-auto min-h-screen py-28">
+        <Header title="conatct section" color="blue-dark" bgColor="zinc-50" />
+        <form className="mx-auto mt-10 max-w-[800px] space-y-6">
+          <input
+            type="text"
+            className="w-full rounded-lg p-4 shadow-sm"
+            placeholder="User Name"
+          />
+          <input
+            type="text"
+            className="w-full rounded-lg p-4 shadow-sm"
+            placeholder="User Age"
+          />
+          <input
+            type="email"
+            className="w-full rounded-lg p-4 shadow-sm"
+            placeholder="User Email"
+          />
+          <input
+            type="password"
+            className="w-full rounded-lg p-4 shadow-sm"
+            placeholder="User Password"
+          />
+          <button
+            type="button"
+            className="bg-green-light rounded-lg p-2 text-zinc-100 shadow-sm"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+      f
+    </section>
+  );
 }
